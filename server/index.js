cf8559692c24c8341c3f96e75b732cb461257cf1
iriyamakotoe <iriyamakotoe@icloud.com>
2024-08-17 11:29:24 +0900

    04_webpack・babelを設定してサーバをビルドできるようにしよう

--- server/index.js
+++ server/index.js
@@ -4,8 +4,24 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-	res.send('Hello, world!')
+	const text = 'Hello, World!'
+	const html = `
+  <html>
+    <head>
+      <title>CountUp</title>
+      <meta charset="utf-8"/>
+    </head>
+    <body>
+      <div id="root">
+	  ${text}
+      </div>
+    </body>
+  </html>
+`
+	res.send(html)
 })
+
+app.use(express.static('./dist'))
 
 app.listen(port, () => {
 	console.log(`port ${port} でサーバー起動中`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,24 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-import process from 'process'
-import path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import nodeExternals from 'webpack-node-externals'
-import {fileURLToPath} from 'url'
+const path = require('path')
 
-const __filename = fileURLToPath(import.meta.url)
-
-const __dirname = path.dirname(__filename)
-
-const isProduction = process.env.NODE_ENV == 'production'
-
-const stylesHandler = 'style-loader'
-
-const config = {
-	entry: './server/index.js',
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'server/index.js'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
+		filename: 'server.js',
 	},
-	devServer: {
-		static: {
-			directory: path.resolve(__dirname, 'dist'),
-		},
-		open: true,
-		host: 'localhost',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'index.html',
-		}),
-		// Add your plugins here
-		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
-	],
 	module: {
 		rules: [
+			// .jsまたは.jsxファイルをBabelでトランスパイルする
 			{
-				// 最初はbabelの配置になる。順番変更不可
-				test: /\.tsx$/i,
-				loader: 'babel-loader',
-				options: {presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']},
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
 			},
-			{
-				test: /\.(ts|tsx)$/i,
-				loader: 'ts-loader',
-				exclude: ['/node_modules/'],
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
-			},
-			{
-				test: /\.css$/i,
-				use: [stylesHandler, 'css-loader', 'postcss-loader'],
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-				type: 'asset',
-			},
-
-			// Add your rules for custom modules here
-			// Learn more about loaders from https://webpack.js.org/loaders/
 		],
 	},
 	resolve: {
-		extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-		alias: {
-			process: 'process/browser',
-		},
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
 	},
-	target: 'node',
-	externals: [nodeExternals()],
-}
-
-export default () => {
-	if (isProduction) {
-		config.mode = 'production'
-	} else {
-		config.mode = 'development'
-	}
-	return config
+	target: ['node', 'es6'],
 }
