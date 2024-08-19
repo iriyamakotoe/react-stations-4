d40a84e484e1c80943400823915628821f7f7e2a
iriyamakotoe <iriyamakotoe@icloud.com>
2024-08-19 09:43:44 +0900

    04_webpack・babelを設定してサーバをビルドできるようにしよう

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {StrictMode} from 'react'
-import {BrowserRouter} from 'react-router-dom'
-import {Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {Home} from './pages/Home'
 import {List} from './pages/List'
 import './App.scss'
@@ -10,7 +9,7 @@ function App() {
 	return (
 		<>
 			<StrictMode>
-				<BrowserRouter basename={import.meta.env.DEV ? '/' : 'react-stations-4'}>
+				<BrowserRouter basename={import.meta.env.DEV ? 'react-stations-4' : '/'}>
 					<Routes>
 						<Route path={`/`} element={<Home />} />
 						<Route path={`/list`} element={<List />} />
